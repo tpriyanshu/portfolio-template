3affc8e07748dddb735221007b6912cb6e15b560
Priyanshu Tiwari <tpriyanshu90@gmail.com>
2018-12-30 22:47:16 +0530

    create robo friends app

---
+++ precache-manifest.ddfd916282080d5c91d71b1e0a656fc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38af24ef778cfa40d312",
+    "url": "/static/js/main.38af24ef.chunk.js"
+  },
+  {
+    "revision": "71e77d6107a6221e55b4",
+    "url": "/static/js/1.71e77d61.chunk.js"
+  },
+  {
+    "revision": "38af24ef778cfa40d312",
+    "url": "/static/css/main.872f2c8d.chunk.css"
+  },
+  {
+    "revision": "71e77d6107a6221e55b4",
+    "url": "/static/css/1.4853a748.chunk.css"
+  },
+  {
+    "revision": "749ab154ac5a9f92789c75cd3be9ac27",
+    "url": "/index.html"
+  }
+];
